222813354219a5d303a77e2bc137ad24082c13d6
GummyBearStudio <keyboardwarriror@gmail.com>
2019-04-08 12:21:36 +0200

    Project 2 finished

--- fiz2/phproject2.py
+++ fiz2/phproject2.py
@@ -21,12 +21,13 @@ def tuplify(c, nms, tup):
 
 # PHYSICS
 
+MARGIN = 0.005
 ICE = (60.0, 40.0)
 GOAL_WIDTH = 1.0
 G = 10.0
 NAMESIN = ["start", "mass", "radius", "friction", "velocity"]
 NAMESOUT = ["stop", "time"]
-DATA = pandas.read_csv("fiz2\\input.txt", sep=';', header=None, names=NAMESIN).transpose()
+DATA = pandas.read_csv("input.txt", sep=';', header=None, names=NAMESIN).transpose()
 
 """ Ice Rink to file """
 def drawRink(start=None):
@@ -40,9 +41,10 @@ def drawRink(start=None):
         plt.plot(*start, 'go')
 
 def checkGoal(pos):
-    upper = (ICE[1]+GOAL_WIDTH) / 2
-    lower = (ICE[1]-GOAL_WIDTH) / 2
-    return (pos[0] <= 0 or pos[0] >= ICE[0]) and pos[1] < upper and pos[1] > lower
+    goalmargin = 0.1
+    upper = ((ICE[1]+GOAL_WIDTH) / 2) + goalmargin
+    lower = ((ICE[1]-GOAL_WIDTH) / 2) - goalmargin
+    return (pos[0] <= goalmargin or pos[0] >= ICE[0]-goalmargin) and pos[1] < upper and pos[1] > lower
 
 class LineMovement:
     def __init__(self, start, v, r, f):
@@ -56,46 +58,50 @@ class LineMovement:
         self.VNorm = (v[0]**2.0 + v[1]**2.0)**0.5
         if self.Velocity[0] != 0.0:
             self.Angle = math.atan(self.Velocity[1] / self.Velocity[0])
+            if self.Velocity[0] < 0: # atan needs vector direction fix when Vx < 0
+                self.Angle = math.pi + self.Angle
         else:
             self.Angle = math.pi / 2
-        self.FVector = (self.Friction*G*math.cos(self.Angle), self.Friction*G*math.sin(self.Angle))
+            if self.Velocity[1] < 0:
+                self.Angle = -self.Angle
+        self.FVector = (-self.Friction*G*math.cos(self.Angle), -self.Friction*G*math.sin(self.Angle))
 
     def update(self, t, b):
         self.Begin = self.getPosition(t)
-        self.setVelocity(self.getVelocity(t))
+        new_v = self.getVelocity(t)
         if b == 'l':
-            self.Velocity = (abs(self.Velocity[0]), self.Velocity[1])
+            new_v = (abs(new_v[0]), new_v[1])
         elif b == 'r':
-            self.Velocity = (-abs(self.Velocity[0]), self.Velocity[1])
+            new_v = (-abs(new_v[0]), new_v[1])
         elif b == 't':
-            self.Velocity = (self.Velocity[0], -abs(self.Velocity[1]))
+            new_v = (new_v[0], -abs(new_v[1]))
         elif b == 'b':
-            self.Velocity = (self.Velocity[0], abs(self.Velocity[1]))
+            new_v = (new_v[0], abs(new_v[1]))
+        self.setVelocity(new_v)
 
     def borders(self):
         return {'l':self.Radius, 'r':ICE[0]-self.Radius, 'b':self.Radius, 't':ICE[1]-self.Radius}
 
     def getHitTime(self):
-        def inrange(t):
-            pos = self.getPosition(t)
-            return pos[0] >= 0 and pos[1] >= 0 and pos[0] <= ICE[0] and pos[1] <= ICE[1]
         borders = self.borders()
         x = sympy.Symbol('x', real=True, positive=True)
         result = self.getStopTime()
         bouncewall = None
         for wall in borders:
             axle = 0 if wall in ['l', 'r'] else 1
-            solutions = sympy.solve(self.getPosition(x)[axle] - borders[wall], x)
+            solutions = sympy.solve(self.getPosition(x+10e-14)[axle] - borders[wall], x)
             if len(solutions) > 0:
-                m = min(solutions)
-                if m < result and inrange(float(m)):
-                    result = float(m)
-                    bouncewall = wall
+                m = float(min(solutions))
+                if m < result:
+                    pos = self.getPosition(m)
+                    if pos[0] >= -MARGIN and pos[1] >= -MARGIN and pos[0] <= ICE[0]+MARGIN and pos[1] <= ICE[1]+MARGIN:
+                        result = m
+                        bouncewall = wall
         self.update(result, bouncewall)
         return result
 
     def getVelocity(self, t):
-        return (self.Velocity[0] - t*self.FVector[0], self.Velocity[1] - t*self.FVector[1])
+        return (self.Velocity[0] + t*self.FVector[0], self.Velocity[1] + t*self.FVector[1])
 
     def getStopTime(self):
         return self.VNorm / (self.Friction*G)
@@ -117,21 +123,22 @@ def main(index, fhandle):
     # params
     finish = sim.getStopTime()
     bounces = [column['start']]
+    times = [0.0]
     elapsed = 0.0
     inside = True
     # loop every bounce
     print("Slide duration: {} s".format(finish))
     while elapsed < finish and inside:
-        traveltime = sim.getHitTime()
-        elapsed += traveltime
+        times.append(sim.getHitTime())
+        elapsed += times[-1]
         bounces.append(sim.Begin)
-        if traveltime==0:
+        if times[-1]==0:
             break
         else:
             inside = not checkGoal(bounces[-1])
-    df = pandas.DataFrame(bounces)
+    df = pandas.DataFrame(data={'x':[round(b[0], 2) for b in bounces], 'y':[round(b[1], 2) for b in bounces], 'delta t':[round(t, 3) for t in times]})
     print(df)
-    plt.plot(df[0], df[1])
+    plt.plot(df['x'], df['y'])
     plt.savefig("{}.png".format(index+1))
     if inside:
         fhandle.write('({0:.2f}, {1:.2f}); {2:.2f}; {3}\n'.format(*bounces[-1], finish, '; '.join(["({0:.2f}, {0:.2f})".format(*bounces[x]) for x in range(1, len(bounces)-1)])))
