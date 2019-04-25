7c219fd58bfa3986f487c685c9e1e09140274a15
GummyBearStudio <keyboardwarriror@gmail.com>
2019-04-25 11:34:30 +0200

    Recycled all possible code

--- fiz3/phproject3.py
+++ fiz3/phproject3.py
@@ -22,34 +22,33 @@ def tuplify(c, nms, tup):
 # PHYSICS
 
 MARGIN = 0.005
-ICE = (60.0, 40.0)
-GOAL_WIDTH = 1.0
-G = 10.0
-NAMESIN = ["start", "mass", "radius", "friction", "velocity"]
-NAMESOUT = ["stop", "time"]
+BALL_MASS = 0.017
+BALL_R = 0.03
+FRICTION = 0.015
+ENERGY_RATIO = 1.0 - 0.3
+TABLE = (2.7, 1.35)
+HOLE_R = 0.1 / 2.0
+G = 9.81
+NAMESIN = ["white", "velocity", "colored"]
+NAMESOUT = ["white", "colored", "bounces", "whitewallhits", "colorwallhits"]
 DATA = pandas.read_csv("input.txt", sep=';', header=None, names=NAMESIN).transpose()
 
-""" Ice Rink to file """
-def drawRink(start=None):
+""" Table to file """
+def draw(markers=None):
     plt.clf()
-    plt.xlim(0, ICE[0])
-    plt.ylim(0, ICE[1])
-    goalratio = GOAL_WIDTH / (2*ICE[1])
-    plt.axvline(0.1, color='g', linewidth=2, ymin=0.5-goalratio, ymax=0.5+goalratio)
-    plt.axvline(ICE[0]-0.1, color='g', linewidth=2, ymin=0.5-goalratio, ymax=0.5+goalratio)
-    if start!=None:
-        plt.plot(*start, 'go')
+    plt.xlim(0, TABLE[0])
+    plt.ylim(0, TABLE[1])
+    if markers!=None:
+        plt.plot(*markers, 'go')
 
+""" Check whether ball well into a hole """
 def checkGoal(pos):
     goalmargin = 0.1
-    upper = ((ICE[1]+GOAL_WIDTH) / 2) + goalmargin
-    lower = ((ICE[1]-GOAL_WIDTH) / 2) - goalmargin
-    return (pos[0] <= goalmargin or pos[0] >= ICE[0]-goalmargin) and pos[1] < upper and pos[1] > lower
+    pass
 
 class LineMovement:
-    def __init__(self, start, v, r, f):
-        self.Radius = r
-        self.Friction = f
+    def __init__(self, start, v):
+        self.borders = {'l':BALL_R, 'r':TABLE[0]-BALL_R, 'b':BALL_R, 't':TABLE[1]-BALL_R}
         self.Begin = start
         self.setVelocity(v)
 
@@ -58,17 +57,20 @@ class LineMovement:
         self.VNorm = (v[0]**2.0 + v[1]**2.0)**0.5
         if self.Velocity[0] != 0.0:
             self.Angle = math.atan(self.Velocity[1] / self.Velocity[0])
-            if self.Velocity[0] < 0: # atan needs vector direction fix when Vx < 0
+            # atan needs vector direction fix when Vx < 0
+            if self.Velocity[0] < 0:
                 self.Angle = math.pi + self.Angle
         else:
             self.Angle = math.pi / 2
             if self.Velocity[1] < 0:
                 self.Angle = -self.Angle
-        self.FVector = (-self.Friction*G*math.cos(self.Angle), -self.Friction*G*math.sin(self.Angle))
+        self.FVector = (-FRICTION*G*math.cos(self.Angle), -FRICTION*G*math.sin(self.Angle))
 
     def update(self, t, b):
         self.Begin = self.getPosition(t)
         new_v = self.getVelocity(t)
+        energy_loss = ENERGY_RATIO ** 0.5
+        new_v = (new_v[0]*energy_loss, new_v[1]*energy_loss)
         if b == 'l':
             new_v = (abs(new_v[0]), new_v[1])
         elif b == 'r':
@@ -79,22 +81,18 @@ class LineMovement:
             new_v = (new_v[0], abs(new_v[1]))
         self.setVelocity(new_v)
 
-    def borders(self):
-        return {'l':self.Radius, 'r':ICE[0]-self.Radius, 'b':self.Radius, 't':ICE[1]-self.Radius}
-
     def getHitTime(self):
-        borders = self.borders()
         x = sympy.Symbol('x', real=True, positive=True)
         result = self.getStopTime()
         bouncewall = None
-        for wall in borders:
+        for wall in self.borders:
             axle = 0 if wall in ['l', 'r'] else 1
-            solutions = sympy.solve(self.getPosition(x+10e-14)[axle] - borders[wall], x)
+            solutions = sympy.solve(self.getPosition(x+10e-14)[axle] - self.borders[wall], x)
             if len(solutions) > 0:
                 m = float(min(solutions))
                 if m < result:
                     pos = self.getPosition(m)
-                    if pos[0] >= -MARGIN and pos[1] >= -MARGIN and pos[0] <= ICE[0]+MARGIN and pos[1] <= ICE[1]+MARGIN:
+                    if pos[0] >= -MARGIN and pos[1] >= -MARGIN and pos[0] <= TABLE[0]+MARGIN and pos[1] <= TABLE[1]+MARGIN:
                         result = m
                         bouncewall = wall
         self.update(result, bouncewall)
@@ -104,10 +102,10 @@ class LineMovement:
         return (self.Velocity[0] + t*self.FVector[0], self.Velocity[1] + t*self.FVector[1])
 
     def getStopTime(self):
-        return self.VNorm / (self.Friction*G)
+        return self.VNorm / (FRICTION*G)
 
     def getDistance(self, t):
-        return self.VNorm * t - 0.5*(t**2.0)*self.Friction*G
+        return self.VNorm * t - 0.5*(t**2.0)*FRICTION*G
 
     def getPosition(self, t):
         distance = self.getDistance(t)
@@ -117,9 +115,9 @@ class LineMovement:
 
 def main(index, fhandle):
     print("-"*30, index+1, "-"*30)
-    column = tuplify(DATA[index], NAMESIN, ["start", "velocity"])
-    drawRink(column['start'])
-    sim = LineMovement(column['start'], column['velocity'], column['radius'], column['friction'])
+    column = tuplify(DATA[index], NAMESIN, NAMESIN)
+    draw(column['start'])
+    sim = LineMovement(column['start'], column['velocity'])
     # params
     finish = sim.getStopTime()
     bounces = [column['start']]
